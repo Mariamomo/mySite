88f3c3cdaa1ecf8cd9af56a78b5fdd75b524f2ae
Mariamomo <50744570+Mariamomo@users.noreply.github.com>
2019-11-17 11:45:21 +0100

    2nd-change

---
+++ index.php
@@ -0,0 +1,342 @@
+<?php include "includes/db.php"
+?>
+<?php include "includes/function.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maria</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<!--jquery-->
+  <script
+  src="http://code.jquery.com/jquery-3.3.1.js"></script>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!--afontawasome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<!--Body Start-->
+<body class="fixed-header-body">
+<!--header start-->
+<header class="header header-style-3 navbar-light">
+  <div class="container">
+     <!--nav-->
+    <nav class="navbar navbar-expand-lg" id="mainNav">
+         <a class="navbar-brand" href="#">Maria</a>
+         <!--Mobile Toggle-->
+         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#header-01" aria-controls="header-01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        <!--Mobile togge end-->
+        </button>
+        <div id="header-01" class="navbar-collapse collapse top-menu">
+             <!--menu start-->
+        <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="#skill">Skill</a></li>
+            <li class="nav-item"><a class="nav-link" href="#eduaction">Education</a></li>
+            <li class="nav-item"><a class="nav-link" href="#career">Career</a></li>
+            <li class="nav-item"><a class="nav-link" href="#contact">Contact Me</a></li>
+            <li class="nav-item"><a class="nav-link" href="sampleWork.php">Sample Work</a></li>
+        </ul>
+        </div>
+     </nav>  
+  </div>  
+</header>
+
+<!--header end-->
+
+<!--main start-->
+<main>
+      <!--home start-->
+       <section id="home">
+          <div class="home-banner-image">
+              <div class="image-container">
+                 <div class="filter"></div>
+                  <img class="fillWidth" src="image/laptop-1483974_640.jpg" alt="image1">
+                             <div class="container text-center">
+                                 <div class="banner-center-text">
+                                    <label class="label">Hello</label>
+                                 <h1 class="text">
+                                    <span class="text1">
+                                        I'm
+                                    </span>
+                                    Maria Ferdous Momo
+                                 </h1>
+                                    <h1 class="text">WEB DEVELOPER</h1>
+
+                                 </div>
+                             </div> 
+              </div>
+          </div>
+           
+       </section>
+<!--home end-->
+        <!--about us start-->
+<section id="about" class="section">
+           <div class="container">
+               <div class="row">
+                   <div class="col-md-5 align-self-center">
+                       <img src="image/maria.jpg" alt="maria" class="img-fluid">
+                   </div>
+                   <div class="col-sm-12 col-md-7 ">
+                       <h5 class="font-alt">Hello! I'm Maria</h5>
+                       <div class="table-responsive-sm">
+  <table class="table">
+   <tr>
+                                   <td>NAME</td>
+                                   <td>:</td>
+                                   <td>MARIA FERDOUS MOMO</td>
+                               </tr>
+                               <tr>
+                                   <td>AGE</td>
+                                   <td>:</td>
+                                   <td>24</td>
+                               </tr>
+                               <tr>
+                                   <td>PROFESSION</td>
+                                   <td>:</td>
+                                   <td>Student</td>
+                               </tr>
+                               <tr>
+                                   <td>PHONE</td>
+                                   <td>:</td>
+                                   <td>+49 1522 1789636</td>
+                               </tr>
+                               <tr>
+                                   <td>EMAIL</td>
+                                   <td>:</td>
+                                   <td>mariamomo6810@gmail.com</td>
+                               </tr>
+  </table>
+</div>
+                    <div> 
+                          <ul class="ul">
+                          <li class="li"><a class="face"  href="https://www.facebook.com/nil.oporajita.357"><i class="fab fa-facebook-f facebook"></i></a></li>
+                          <li class="li"><a class="goog" href="#" ><i class="fab fa-google google"></i></a></li>
+                          <li class="li"><a class="link" href="https://www.linkedin.com/in/
+mariaferdousmomo-42b359173/"><i class="fab fa-linkedin-in linkdin"></i></a></li>
+                          <li class="li" style="padding-right: 0px;"><a class="xin" href="https://www.xing.com/profile/MariaFerdous_Momo/cv"><i class="fab fa-xing xing"></i></a></li>
+                      </ul> 
+                    </div>
+                    
+                       </div>
+                       </div>
+                       </div>
+    
+            
+        </section>
+        <!--about us end-->
+        <!--skill start-->
+        <section id="skill" class="section grid" style="background: none">
+          <div class="container">
+            <h5 class="font-alt text-center text-uppercase font-w-600">
+                    skill   
+                   </h5>
+             <div class="row justify-content-center">
+                <div class="col-md-6 text-center">
+                    <div class="skills col-md-6 col-lg-12">
+                        <li>
+                        <h6 class="h6">HTML5</h6>
+                        <span class="bar"><span class="html"></span><div class="percent">70%</div></span>
+                    </li>
+                    <li>
+                        <h6>CSS3</h6>
+                        <span class="bar"><span class="css"></span><div class="percent">70%</div></span>
+                    </li>
+                    <li>
+                        <h6>PHP</h6>
+                        <span class="bar"><span class="php"></span><div class="percent">50%</div></span>
+                    </li>
+                    <li>
+                        <h6>Mysql(phpMyAdmin)</h6>
+                        <span class="bar"><span class="php"></span><div class="percent">50%</div></span>
+                    </li>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center">
+                    <div class="skills col-md-6 col-lg-12">
+                      <li>
+                       <h6>BOOTSTRAP</h6>
+                        <span class="bar"><span class="bootstrap"></span><div class="percent">70%</div></span>
+                    </li>
+                    <li>
+                        <li>
+                        <h6>JAVASCRIPT</h6>
+                        <span class="bar"><span class="javascript"></span><div class="percent">40%</div></span>
+                    </li>
+                
+                        <li>
+                        <h6>WORDPRESS</h6>
+                        <span class="bar"><span class="wordpress"></span><div class="percent">30%</div></span>
+                    </li>
+                    <li>
+                        <h6>Jquery</h6>
+                        <span class="bar"><span class="jquery"></span><div class="percent">30%</div></span>
+                    </li>
+                    </div>
+                </div>
+                 
+             </div>
+              
+          </div> 
+        </section>
+        <!--skill end-->
+        <!--education start-->
+        <section id="eduaction" class="section">
+            <div class="row ustify-content-center">
+                <div class="container">
+                    <div class="tree">
+         <ul>
+            <li>
+                <a class="aa" href=""><h3>Education</h3></a>
+                <ul>
+                    <li>
+                        <a href="https://www.seu.ac.bd" title="some title" id="link1">
+                        <h2>Bachelor's</h2>
+                        <h3>Southeast University of Bangladeh</h3>
+                        <br>
+                        <h4>Computer Science And Engineering</h4>
+                        <h5>2013-2017</h5>
+                        </a>
+                        
+                    </li>
+                    <li>
+                       <a href="https://www.th-koeln.de" title="some title" id="link">
+                        <h2>Master's</h2>
+                        <h3>Cologne University of Applied Science</h3>
+                        <br>
+                        <h4>Communication Systems And Networks</h4>
+                        <h5>2018-Present</h5>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+                </div>
+            </div>
+               
+            
+        </section>
+        
+        <!--education end-->
+                
+        <!--career start-->
+        <section id="career">
+            <div class="container mb-5">
+               <div class="main-title">
+               <h2 class="text-center font-alt pt-5 pb-3">CAREER HISTORY</h2>
+               </div>
+               <div class="row justify-content-center">
+                <div class="col-12 col-md-6 mb-3">
+                    <div class=" wow fadeIn animated" style="visibility: visible; animation-name: fadeIn; ">
+                     <div class="service_item" >
+                         <i class="fas fa-code"></i> 
+                     <a style="text-decoration: none; background-color: transparent; color: darkorange;" href="#">
+                         <h4>Junior Web Application Developer</h4>
+                     </a>  
+                     <h4 style="padding: 5px 0px 15px 0px; color: darkorange">Amarsoft Solutions Ltd.</h4>
+                     <h5 style="color: aqua">October-2017 to August-2018</h5>
+                     </div>
+                     <br>
+                     <h5 style="color: aqua">My Duties were:</h5>
+                     <ul style="color: aquamarine;">
+                        <li class="list">Develop web with HTML5, CSS3, PHP, Bootstrap4 and JavaScript.</li> 
+                        <li class="list">Work with relation database MySQL, PhpMyAdmin</li> 
+                        <li class="list">Identify device problem and devise elegant solutions</li> 
+                        <li class="list">Design UI for website and desktop web applications</li> 
+                     </ul> 
+                    </div>
+                </div>   
+                <div class="col-12 col-md-6 mb-3">
+                  <div class="wow fadeIn animated" style="visibility: visible; animation-name: fadeIn; ">
+                     <div class="service_item ">
+                         <i class="fas fa-laptop"></i>
+                     <a style="text-decoration: none; background-color: transparent; color: black;" href="#">
+                         <h4 style="color: darkorange">Internship</h4>
+                     </a>  
+                     <h4 style="padding: 5px 0px 15px 0px; color: darkorange">Amarsoft Solutions Ltd.</h4>
+                     <h5 style="color: aqua">november-2016 to january-2017</h5>
+                     </div>
+                     <br>
+                     <p style="color: aquamarine">During Internship, I worked and visit all departments and to get a comprehensive understanding of the whole organizatuion.
+                     specially software development and web application developer department</p>
+                    
+                    </div>
+                </div>   
+               </div>
+               </div>
+        </section>
+        <!--career end-->
+        <!--contact me start-->
+        <section class="section grid" id="contact">
+            <div class="container">
+               <div class="main-title">
+               <h2 class="text-center font-alt pt-3 pb-1 pr-3">CONTACT WITH ME</h2>
+               </div>
+          
+               <div class="row justify-content-center align-items-center h-100">
+                    <div class="col-sm-6 col-md-8 col-lg-8 col-xl-8">
+                   <form id="from-group" method="post" action="mail_handeler.php">
+                       <input name="name" type="text" class="form-control" placeholder="Your Name" required> <br>
+                       <input name="email" type="text" class="form-control" placeholder="Your Email" required> <br>
+                       <textarea name="msg" class="form-control" placeholder="Message" rows="4"></textarea>
+                       <br>
+                       <input type="submit" class="form-control submit" value="SEND MESSAGE">
+                   </form>
+               </div>
+               </div>
+                  
+            
+            </div>
+        </section>
+</main>       
+<!--main end-->
+<!--footer start-->
+<!--footer end-->
+        
+<!--javascript-->
+<script type="text/javascript">    
+   $(window).on('scroll', function()
+        {
+       if($(window).scrollTop()){
+           $('header').addClass('fixed-header');
+       }
+       else{
+    $('header').removeClass('fixed-header');
+       }
+   })
+    $(document).ready(function(){
+        $('ul li a').click(function(){
+            $('li a').removeClass("current");
+            $(this).addClass("current");
+        })
+    })
+    </script>
+    <script>
+    $(document).ready(function(){
+       $("#link").attr({
+      "href" : "https://www.th-koeln.de",
+      "title" : "TH Cologne"
+    });
+        $(document).ready(function(){
+       $("#link1").attr({
+      "href" : "https://www.seu.ac.bd",
+      "title" : "Southeast University"
+    });
+});
+</script>
+<script src="//code.tidio.co/61bjp1z0vik8z1zrvn18nd1vvgddhoy3.js"></script>
+</body>
+</html>
+
+    
