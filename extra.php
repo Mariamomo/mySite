6380b0da76a3b2d504d62852240ce622fd1cbc19
Mariamomo <50744570+Mariamomo@users.noreply.github.com>
2019-11-17 11:50:20 +0100

    newfile

---
+++ extra.php
@@ -0,0 +1,17 @@
+<div class="tbl">
+                <div class="table-responsive-sm" style="background-color:white;">
+               <table class="table table-border table-striped">
+        
+          <tr>
+              <h2 style="text-align:center;">user</h2>
+          </tr>
+           <tr>
+               <th>Name</th>
+               <th>Email</th>
+               <th>Age</th>
+               <th>Phonenumber</th>
+           </tr>
+           
+           </table>
+          </div>
+                </div>
